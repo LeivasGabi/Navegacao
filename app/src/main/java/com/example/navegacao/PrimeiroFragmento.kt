59a822e3ae02d540a6e1d49f42cb31c8ea4f9d39
LeivasGabi <leivasgabi@gmail.com>
2022-12-13 15:52:57 -0300

    navegation

--- app/src/main/java/com/example/navegacao/PrimeiroFragmento.kt
+++ app/src/main/java/com/example/navegacao/PrimeiroFragmento.kt
@@ -3,6 +3,7 @@ package com.example.navegacao
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -11,8 +12,18 @@ class PrimeiroFragmento : Fragment(R.layout.fragmento_primeiro) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val nome = view.findViewById<EditText>(R.id.nome)
+        val email = view.findViewById<EditText>(R.id.email)
+        val nascimento = view.findViewById<EditText>(R.id.nascimento)
+
         view.findViewById<Button>(R.id.primeiro).setOnClickListener {
-            findNavController().navigate(R.id.primeiroParaSegundo)
+
+//            val action = PrimeiroFragmentoDirections
         }
     }
 }
+//"${nome.text}",
+//"${email.text}",
+//"${nascimento.text}"
+
+//findNavController().navigate(action)
--- app/src/main/java/com/example/navegacao/SegundoFragmento.kt
+++ app/src/main/java/com/example/navegacao/SegundoFragmento.kt
@@ -5,10 +5,14 @@ import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 class SegundoFragmento : Fragment(R.layout.fragmento_segundo) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
 
         view.findViewById<Button>(R.id.segundo).setOnClickListener {
             findNavController().navigate(R.id.segundoParaTerceiro)
